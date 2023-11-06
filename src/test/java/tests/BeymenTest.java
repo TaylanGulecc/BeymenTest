da490b7e1aefb9f6a0c8f4129ac4c122dee0d090
unknown <gulec.taylan@gmail.com>
2023-11-06 16:47:52 +0300

    beymen urun sepete ekleme ve urunu sepetten silme testi yazılmistir

--- src/test/java/tests/BeymenTest.java
+++ src/test/java/tests/BeymenTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.Browser;
 import pages.HomePage;
 import pages.ProductDetailPage;
 import pages.SearchPage;
@@ -81,84 +80,13 @@ public class BeymenTest {
         shoppingCartPage.setQuantity("2");
         shoppingCartPage.removeFromShoppingCart();
         shoppingCartPage.assertShoppingCartEmpty();
+
+        driver.quit();
     }
 
-
-
-
-
-
-//    @Test
-//    public void beymenTestScenario() throws InterruptedException, IOException {
-//        File fs = new File("src/files/search.xlsx");
-////Creating a workbook
-//        Workbook workbook = WorkbookFactory.create(fs);
-//        Sheet sheet = workbook.getSheetAt(0);
-//        String firstSearch = sheet.getRow(0).getCell(0).toString();
-//        String secondSearch = sheet.getRow(0).getCell(1).toString();
-//
-//        logger.info("Setup kuruluyor");
-//        System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        logger.info("https://www.beymen.com'a gidilir");
-//        driver.get("https://www.beymen.com");
-//        Thread.sleep(2000);
-//        logger.info("Çerez pop-up görüntülenir ve tüm çerezleri kabul et'e tıklanır");
-//        driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
-//        logger.info("cinsiyet seçim pop-up'ı görüntülenir ve cinsiyet seçilir.");
-//        driver.findElement(By.xpath("//button[@id='genderManButton']")).click();
-//        Thread.sleep(2000);
-//        driver.findElement(By.xpath("//input[@class=\"o-header__search--input\"]")).click();
-//        Thread.sleep(2000);
-//        driver.findElement(By.id("o-searchSuggestion__input")).sendKeys(firstSearch);
-//        Thread.sleep(2000);
-//        driver.findElement(By.xpath("//button[contains(text(),'Sil')]")).click();
-//        driver.findElement(By.id("o-searchSuggestion__input")).sendKeys(secondSearch);
-//        driver.findElement(By.id("o-searchSuggestion__input")).sendKeys(Keys.ENTER);
-//        int productCount = driver.findElements(By.xpath("//div[@class=\"o-productList__item\"]")).size();
-//        int randomProduct = new Random().nextInt(productCount);
-//        driver.findElements(By.xpath("//div[@class=\"o-productList__item\"]")).get(randomProduct).click();
-//        String productDescription = driver.findElement(By.className("o-productDetail__description")).getText();
-//        logger.info(productDescription);
-//        String productPriceString = driver.findElement(By.id("priceNew")).getText();
-//
-//        File txtFile = new File("src/files/txtFile.txt");
-//        Files.deleteIfExists(txtFile.toPath());
-////        Files.createFile(txtFile.toPath());
-//        List<String> lines = Arrays.asList(productDescription, productPriceString);
-//        Files.write(txtFile.toPath(), lines, StandardCharsets.UTF_8);
-//
-//        logger.info(productPriceString);
-//        List<WebElement> sizeWebElements = driver.findElements(By.xpath("//span[contains(@class, \"m-variation__item\") and not(contains(@class,'-disabled'))]"));
-//        int randomProductSize = new Random().nextInt(sizeWebElements.size());
-//        sizeWebElements.get(randomProductSize).click();
-//        driver.findElement(By.id("addBasket")).click();
-//        logger.info("ürünün sepete eklendiğinin bildirimi görünür.");
-//        Thread.sleep(2000);
-//        driver.findElement(By.xpath("//span[contains(text(),'Sepetim')]")).click();
-//        Thread.sleep(2000);
-//        String productPriceOnBasketString = driver.findElement(By.className("m-productPrice__salePrice")).getText();
-//        logger.info(productPriceOnBasketString);
-//
-//        try {
-//            NumberFormat format = NumberFormat.getInstance(Locale.forLanguageTag("tr_TR"));
-//            double productPrice = format.parse(productPriceString.replace("TL", "").replace(" ", "")).doubleValue();
-//            double productPriceOnBasket = format.parse(productPriceOnBasketString.replace("TL", "").replace(" ", "")).doubleValue();
-//            Assert.assertTrue("Fiyatlar esit degil", productPrice == productPriceOnBasket);
-//        } catch (ParseException e) {
-//            Assert.fail("Fiyatlar parse edilemedi");
-//        }
-//
-//        driver.findElement(By.id("quantitySelect0-key-0"));
-//        driver.findElement(By.xpath("//option[@value='2']")).click();
-//        Thread.sleep(1000);
-//        driver.findElement(By.id("removeCartItemBtn0-key-0")).click();
-//        Thread.sleep(2000);
-//        driver.findElement(By.xpath("//*[contains(text(),'Sepetinizde Ürün Bulunmamaktadır')]"));
-//
-//
-//    }
-
-
 }
+
+
+
+
+
